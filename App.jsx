c3355d29f3225e46a9a0aaa1fd60c4bf03d0a725
amruta2120 <84914583+amruta2120@users.noreply.github.com>
2021-06-07 12:11:45 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import Box from "./Box";
+function App(){
+  
+    return(
+<div>
+  <Header/>
+  <Footer/>
+  <Box/>
+  </div>
+  );
+}
+
+export default App;
---
+++ Box.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Box()
+{
+  return(
+    <div className="box">
+      <h1>ShapeAi Bootcamp Names :</h1>
+      <ul>
+        <li>Python & Machine Learning</li>
+        <li>Python & Computer Vision</li>
+        <li>Javascript & ReactJs</li>
+        </ul>
+      </div>
+  );
+}
+
+export default Box;
